e154a832a13ed558949f1f1a784df0d4d126af29
kleewaye <katie.leewaye@gmail.com>
2026-07-04 13:38:52 -0600

    Additional class definition mapping for collectors

--- src/aws_audit_collectors/ec2_audit_collector.py
+++ src/aws_audit_collectors/ec2_audit_collector.py
@@ -7,3 +7,41 @@
 #==========================================================================
 # Import base detector
 from .baseCollector import BaseCollector
+from ..AWSStandardizedDataStructures import EC2Inventory
+
+#------------------------
+# Class Definition : EC2Collector
+#------------------------
+class EC2Collector(BaseCollector):
+    # Define the collection checks
+    def collect(self):
+        ec2_inventory = EC2Inventory()
+
+        # Begin collection
+        ec2_inventory.instances = self.collect_instances()
+        ec2_inventory.security_groups = self.collect_security_groups()
+        ec2_inventory.network_interfaces = self.collect_network_interfaces()
+        ec2_inventory.ebs_volumes = self.collect_ebs_volumes()
+        ec2_inventory.metadata_options = self.collect_metadata_options()
+        ec2_inventory.key_pairs = self.collect_key_pairs()
+
+        return ec2_inventory
+
+    # Helper functions
+    def collect_instances(self):
+        pass
+
+    def collect_security_groups(self):
+        pass
+
+    def collect_network_interfaces(self):
+        pass
+
+    def collect_ebs_volumes(self):
+        pass
+
+    def collect_metadata_options(self):
+        pass
+
+    def collect_key_pairs(self):
+        pass
--- src/aws_audit_collectors/s3_audit_collector.py
+++ src/aws_audit_collectors/s3_audit_collector.py
@@ -7,3 +7,46 @@
 #==========================================================================
 # Import base detector
 from .baseCollector import BaseCollector
+from ..AWSStandardizedDataStructures import S3Inventory
+
+#------------------------
+# Class Definition : S3Collector
+#------------------------
+class S3Collector(BaseCollector):
+    # Define the collection checks
+    def collect(self):
+        s3_inventory = S3Inventory()
+
+        # Begin collection
+        s3_inventory.buckets = self.collect_buckets()
+        s3_inventory.bucket_policies = self.collect_bucket_policies()
+        s3_inventory.acls = self.collect_acls()
+        s3_inventory.public_access_block = self.collect_public_access_block()
+        s3_inventory.encryption = self.collect_encryption()
+        s3_inventory.versioning = self.collect_versioning()
+        s3_inventory.logging = self.collect_logging()
+
+        return s3_inventory
+
+    # Helper Functions
+    def collect_buckets(self):
+        pass
+
+    def collect_bucket_policies(self):
+        pass
+
+    def collect_acls(self):
+        pass
+
+    def collect_public_access_block(self):
+        pass
+
+    def collect_encryption(self):
+        pass
+
+    def collect_versioning(self):
+        pass
+
+    def collect_logging(self):
+        pass
+
