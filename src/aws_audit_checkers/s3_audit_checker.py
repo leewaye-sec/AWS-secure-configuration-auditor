1217a4fb96e400001012490b69717189a0aac8ad
kleewaye <katie.leewaye@gmail.com>
2026-07-09 21:27:35 -0600

    Development of S3 collection class and results. Notes and organization for S3 checker class.

--- src/aws_audit_checkers/s3_audit_checker.py
+++ src/aws_audit_checkers/s3_audit_checker.py
@@ -8,46 +8,70 @@
 # Import base detector
 from .baseChecker import BaseChecker
 from ..AWSStandardizedDataStructures import AuditFinding
+from ..AWSStandardizedDataStructures import S3Inventory
 
 #------------------------
 # Class Definition : PublicBucketCheck
 #------------------------
 class PublicBucketCheck(BaseChecker):
-    def process(self, inventory):
+    def process(self, inventory: S3Inventory):
         pass
+    # Notes:
+    #   try:
+    #       pab_configuration = bucket_pab.get('PublicAccessBlockConfiguration')
+    #   except ClientError as e:
+    #       if e.response["Error"]["Code"] == "NoSuchPublicAccessBlockConfiguration":
+    #           Unconfigured / disabled
 
 #------------------------
 # Class Definition : BucketEncryptionCheck
 #------------------------
 class BucketEncryptionCheck(BaseChecker):
-    def process(self, inventory):
+    def process(self, inventory: S3Inventory):
         pass
+    # Notes:
+    #   Encryption rule and defaults:
+    #   try:
+    #       encryption_rule = bucket_encryption["ServerSideEncryptionConfiguration"]["Rules"][0]
+    #       encryption_defaults = encryption_rule.get("ApplyServerSideEncryptionByDefault", {})
+    #   except ClientError as e:
+    #       if e.response["Error"]["Code"] == "ServerSideEncryptionConfigurationNotFoundError":
+    #           # Encryption is disabled/default
 
 #------------------------
 # Class Definition : BucketVersioningCheck
 #------------------------
 class BucketVersioningCheck(BaseChecker):
-    def process(self, inventory):
+    def process(self, inventory: S3Inventory):
         pass
+    # Notes: bucket.Versioning().status
 
 #------------------------
 # Class Definition : BucketLoggingCheck
 #------------------------
 class BucketLoggingCheck(BaseChecker):
-    def process(self, inventory):
+    def process(self, inventory: S3Inventory):
         pass
+    # Notes:
+    #   Checks:
+    #       - Logging configuration removed or missing  (logging == None)
+    #       - Logging disabled
+    #           try:
+    #               bucket_logging.load()
+    #               if bucket_logging.logging_enabled:
+    #               ...
 
 #------------------------
 # Class Definition : BucketACLCheck
 #------------------------
 class BucketACLCheck(BaseChecker):
-    def process(self, inventory):
+    def process(self, inventory: S3Inventory):
         pass
 
 #------------------------
 # Class Definition : BucketOwnershipCheck
 #------------------------
 class BucketOwnershipCheck(BaseChecker):
-    def process(self, inventory):
+    def process(self, inventory: S3Inventory):
         pass
 
--- src/aws_audit_collectors/s3_audit_collector.py
+++ src/aws_audit_collectors/s3_audit_collector.py
@@ -6,6 +6,8 @@
 #
 #==========================================================================
 # Import base detector
+import boto3
+from botocore.exceptions import ClientError
 from .baseCollector import BaseCollector
 from ..AWSStandardizedDataStructures import S3Inventory
 
@@ -14,39 +16,167 @@ from ..AWSStandardizedDataStructures import S3Inventory
 #------------------------
 class S3Collector(BaseCollector):
     # Define the collection checks
-    def collect(self):
+    def collect(self, session):
         s3_inventory = S3Inventory()
 
+        # Gather S3 client / resources
+        s3_client = session.client('s3')
+        s3_resources = session.resource('s3')
+
         # Begin collection
-        s3_inventory.buckets = self.collect_buckets()
-        s3_inventory.bucket_policies = self.collect_bucket_policies()
-        s3_inventory.acls = self.collect_acls()
-        s3_inventory.public_access_block = self.collect_public_access_block()
-        s3_inventory.encryption = self.collect_encryption()
-        s3_inventory.versioning = self.collect_versioning()
-        s3_inventory.logging = self.collect_logging()
+        s3_inventory.buckets = self.collect_buckets(s3_resources)
+        s3_inventory.bucket_policies = self.collect_bucket_policies(s3_resources)
+        s3_inventory.acls = self.collect_acls(s3_resources)
+        s3_inventory.public_access_block = self.collect_public_access_block(s3_resources, s3_client)
+        s3_inventory.encryption = self.collect_encryption(s3_resources, s3_client)
+        s3_inventory.versioning = self.collect_versioning(s3_resources)
+        s3_inventory.logging = self.collect_logging(s3_resources)
 
         return s3_inventory
 
     # Helper Functions
-    def collect_buckets(self):
-        pass
+    def collect_buckets(self, resource):
+        return resource.buckets.all()
 
-    def collect_bucket_policies(self):
-        pass
+    # Return list of dictionaries
+    def collect_bucket_policies(self, resource):
+        # Gather buckets
+        buckets = resource.buckets.all()
+        collected_policies = []
+        # Loop through and grab bucket name and policies
+        for bucket in buckets:
+            name = bucket.name
+            policy_response = resource.BucketPolicy(name)
+            bucket_policy = {
+                "bucket_name": name,
+                "bucket_policy": policy_response
+            }
+            collected_policies.append(bucket_policy)
 
-    def collect_acls(self):
-        pass
+        return collected_policies
 
-    def collect_public_access_block(self):
-        pass
+    def collect_acls(self, resource):
+        # Gather buckets
+        buckets = resource.buckets.all()
+        collected_acls = []
+        # Loop through and grab bucket name and policies
+        for bucket in buckets:
+            name = bucket.name
+            # Grab acl policy
+            bucket_acl = bucket.Acl()
 
-    def collect_encryption(self):
-        pass
+            # If acl was returned
+            if bucket_acl:
+                acl = {
+                    "bucket_name" : name,
+                    "bucket_acl" : bucket_acl
+                }
+                collected_acls.append(acl)
+            else:
+                acl = {
+                    "bucket_name": name,
+                    "bucket_acl": None
+                }
+                collected_acls.append(acl)
 
-    def collect_versioning(self):
-        pass
+        return collected_acls
 
-    def collect_logging(self):
-        pass
+    def collect_public_access_block(self, resource, client):
+        # Gather buckets
+        buckets = resource.buckets.all()
+
+        collected_pabs = []
+        # Iterate through the buckets and use client to get public access block
+        for bucket in buckets:
+            name = bucket.name
+
+            try:
+                bucket_pab = client.get_public_access_block(Bucket=name)
+                pab = {
+                    "bucket_name": name,
+                    "bucket_pab": bucket_pab
+                }
+                collected_pabs.append(pab)
+            except ClientError as e:
+                pab = {
+                    "bucket_name" : name,
+                    "bucket_pab" : None
+                }
+                collected_pabs.append(pab)
+
+        return collected_pabs
+
+    def collect_encryption(self, resource, client):
+        # Gather buckets
+        buckets = resource.buckets.all()
+
+        collected_encryption_blocks = []
+        for bucket in buckets:
+            name = bucket.name
+
+            try:
+                enc_block = client.get_bucket_encryption(Bucket=name)
+                encryption_block = {
+                    "name": name,
+                    "bucket_encryption": enc_block
+                }
+                collected_encryption_blocks.append(encryption_block)
+
+            except ClientError as e:
+                encryption_block = {
+                    "name": name,
+                    "bucket_encryption": None
+                }
+                collected_encryption_blocks.append(encryption_block)
+
+        return collected_encryption_blocks
+
+
+    def collect_versioning(self, resource):
+        # Gather buckets
+        buckets = resource.buckets.all()
+
+        collected_versioning = []
+        for bucket in buckets:
+            name = bucket.name
+            version = bucket.Versioning()
+
+            if version:
+                bucket_version = {
+                    "name": name,
+                    "bucket_versioning": version
+                }
+                collected_versioning.append(bucket_version)
+            else:
+                bucket_version = {
+                    "name": name,
+                    "bucket_versioning": None
+                }
+                collected_versioning.append(bucket_version)
+
+        return collected_versioning
+
+    def collect_logging(self, resource):
+        # Gather buckets
+        buckets = resource.buckets.all()
+
+        collected_versioning = []
+        for bucket in buckets:
+            name = bucket.name
+            logging = resource.BucketLogging(name)
+
+            if logging:
+                bucket_logging = {
+                    "name": name,
+                    "bucket_logging": logging
+                }
+                collected_versioning.append(bucket_logging)
+            else:
+                bucket_logging = {
+                    "name": name,
+                    "bucket_logging": None
+                }
+                collected_versioning.append(bucket_logging)
+
+        return collected_versioning
 
